ad7862493dd0269318d878b8337d35381e99f7f1
Franco Canevali <franco.e.canevali@accenture.com>
2019-09-09 09:31:17 -0300

    clean start

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAIKTXAKHZh4-UXru7FaG7IHkAR3Mx4veA",
-    authDomain: "saladejuegos-d829d.firebaseapp.com",
-    databaseURL: "https://saladejuegos-d829d.firebaseio.com",
-    projectId: "saladejuegos-d829d",
-    storageBucket: "saladejuegos-d829d.appspot.com",
-    messagingSenderId: "857042851772",
-    appId: "1:857042851772:web:2fe99d6e37a85cab"
-  }
-  //firebase.initializeApp(firebaseConfig);
+  production: true
 };
